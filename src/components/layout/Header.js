2db0b46c005013cc5f8c9fbb4ab8e43c5114340e
suld2495 <suld7777@gmail.com>
2023-02-27 23:10:20 +0900

    헤더 추가 수정

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,12 @@
 import Menu from "./Menu";
 import styles from './header.module.css';
 import { Helmet } from "react-helmet";
-import { URL } from "../../config";
+import { THUMNAIL, URL } from "../../config";
 
 export default function Header() {
+  const title = "9UP 프로야구";
+  const description = "진정한 야덕을 위한 KBO 덕질 플랫폼, 9UP 프로야구!";
+
   return (
     <header className={styles.header}>
       <Helmet>
@@ -13,19 +16,19 @@ export default function Header() {
         <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />
         <meta http-equiv="expires" content="0" />
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
-        <meta name="title" content="9UP 프로야구" />
-        <meta name="description" content="진정한 야덕을 위한 KBO 덕질 플랫폼, 9UP 프로야구!" />
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
         <meta name="keywords" content="9up, 라인업, 나인업, 컬렉터블, 프로야구, kbo, 한국야구, 야구게임, 야구카드, 시뮬레이션, 스포츠, 야구, 매니지먼트, 매니징, 야구응원가, kbo프로야구, 비더레전드, 컴프야, 컴프매, 넷프야, 마구마구" />
-        <meta property="image" content={URL} />
-        <meta property="og:url" class="fb_url" content="" />
-        <meta property="og:site_name" content="9UP 프로야구" />
-        <meta property="og:title" class="fb_title" content="9UP 프로야구" />
+        <meta property="image" content={THUMNAIL} />
+        <meta property="og:url" class="fb_url" content={URL} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:title" class="fb_title" content={title} />
         <meta property="og:type" content="website" />
-        <meta property="og:keywords" content="9UP 프로야구" />
-        <meta property="og:description" content="진정한 야덕을 위한 KBO 덕질 플랫폼, 9UP 프로야구!" />
-        <meta property="og:image" content={URL} />
-        <meta property="og:image:width" content="680" />
-        <meta property="og:image:height" content="356" />
+        <meta property="og:keywords" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={THUMNAIL} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Helmet>
       <div className={styles.logo}><img src='/images/logo_top.png' alt="logo" /></div>
       <Menu />
--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
 export const GOOGLE_APP_URL = 'https://9up.onelink.me/TEEg/1st9up';
 export const IOS_APP_URL = 'https://9up.onelink.me/TEEg/1st9up';
-export const URL = 'https://testest-sable.vercel.app/thumnail.jpg';
+export const URL = 'https://testest-sable.vercel.app';
+export const THUMNAIL = `${URL}/thumnail.jpg`;
